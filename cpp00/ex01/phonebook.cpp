19633098edddf12dd0bdb90792f86655ffb11718
Jorge Vacarisas bofill <jvacaris@c3r2s2.42madrid.com>
2022-05-18 21:57:04 +0200

    Continued with ex01

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -6,22 +6,28 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 20:44:04 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/05/17 20:58:53 by jvacaris         ###   ########.fr       */
+/*   Updated: 2022/05/18 21:55:24 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
+Contact::Contact(std::string _name, std::string _surname, std::string _nickname, int phone, std::string)
+
 int main()
 {
-	std::string command;
+	Contact		itemlist[8];
+	std::string	command;
+	int			num_items = 0;
 	while (42)
 	{
 		std::cin >> command;
 		if (!command.compare("EXIT"))
 			return(0);
-
-
+		else if (!command.compare("SEARCH"))
+			pb_search(command, itemlist, num_items);
+		else if (!command.compare("ADD"))
+			pb_add(command, itemlist, &num_items);
 
 	}
 }
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -6,9 +6,22 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 20:48:15 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/05/17 20:49:08 by jvacaris         ###   ########.fr       */
+/*   Updated: 2022/05/18 21:55:34 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+class Contact
+{
+	private:
+		std::string	name;
+		std::string	surname;
+		std::string	nickname;
+		int			phone;
+		std::string	secret;
+	public:
+		Contact(std::string, std::string, std::string, int, std::string)
+		int valid_name(std::string name);
+};
