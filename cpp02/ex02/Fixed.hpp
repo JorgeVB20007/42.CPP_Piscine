4bc2c30cced9f390dcd3953c5eca10d106429018
Jorge Vacarisas bofill <jvacaris@c3r2s6.42madrid.com>
2022-09-07 22:47:35 +0200

    02-01 should work·

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 21:39:25 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/09/07 21:58:14 by jvacaris         ###   ########.fr       */
+/*   Updated: 2022/09/07 22:38:37 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,25 @@ class Fixed
 		Fixed();
 		~Fixed();
 		Fixed( Fixed const &tocopy );
-		Fixed( const int inumber );		//* NEW
-		Fixed( const float fnumber );	//* NEW
+		Fixed( const int inumber );
+		Fixed( const float fnumber );
 		Fixed & operator = (Fixed const &toequalize);
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
-		float toFloat( void ) const;	//* NEW
-		int toInt( void ) const;		//* NEW
+		float toFloat( void ) const;
+		int toInt( void ) const;
+		bool & operator > (Fixed const &second);
+		bool & operator < (Fixed const &second);
+		bool & operator >= (Fixed const &second);
+		bool & operator <= (Fixed const &second);
+		bool & operator == (Fixed const &second);
+		bool & operator != (Fixed const &second);
 	private:
 		int _fpv;
 		static const int _frac_bits = 8;
 	
 };
 
-std::ostream & operator << (std::ostream &output, Fixed const &toequalize);	//* NEW
+std::ostream & operator << (std::ostream &output, Fixed const &toequalize);
 
 #endif
