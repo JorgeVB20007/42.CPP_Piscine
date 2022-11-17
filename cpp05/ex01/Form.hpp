adf89c5dfeded8288da087c38a1afdf787eb466e
jvacaris <>
2022-11-17 20:23:12 +0100

    05-00 done

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 20:09:18 by jvacaris          #+#    #+#             */
+/*   Updated: 2022/11/17 20:22:04 by jvacaris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include "Bureaucrat.hpp"
+
+class Form
+{
+	private:
+		const std::string name;
+		bool is_signed;
+		const int min_grade;
+};
+
+#endif
