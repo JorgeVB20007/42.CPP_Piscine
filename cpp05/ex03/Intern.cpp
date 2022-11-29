de6fc42920e442de30d9fa1713e67f9cf7608b8c
jvacaris <>
2022-11-29 20:28:12 +0100

    05-02 should be done

---
+++ cpp05/ex03/Intern.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 19:42:24 by jvacaris          #+#    #+#             */
+/*   Updated: 2022/11/29 20:22:44 by jvacaris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Intern.hpp"
+
+Intern::Intern()
+{
+	if (NOTIFS)
+		std::cout << "Intern Default constructor called." << std::endl;
+}
+
+Intern::Intern(Intern &tocopy)
+{
+	if (NOTIFS)
+		std::cout << "Intern Copy constructor called." << std::endl;
+}
+
+Intern::~Intern()
+{
+	if (NOTIFS)
+		std::cout << "Intern Default destructor called." << std::endl;
+}
+
+Intern & Intern::operator = (Intern &toequalize)
+{
+	if (NOTIFS)
+		std::cout << "Intern Assignation operator called." << std::endl;
+	return (*this);
+}
+
+static std::string str_toLower(std::string oldstr)
+{
+	std::string newstr;
+	int a = -1;
+
+	while (oldstr[++a])
+	{
+		if (oldstr[a] >= 'A' && oldstr[a] <= 'Z' )
+			newstr[a] = oldstr[a] + 32;
+		else
+			newstr[a] = oldstr[a];
+	}
+	return(newstr);
+}
+
+static int find_coincidence(std::string tosearch)
+{
+	std::string options[] = {"shrubbery", "robotomy", "presidential", "shrubbery creation", "robotomy request", "presidential pardon", "shrubberycreation", "robotomyrequest", "presidentialpardon", "shrubberycreation", "robotomyrequest", "presidentialpardon", "shrubberycreationform", "robotomyrequestform", "presidentialpardonform"};
+	int a = -1;
+
+	while (++a < 15)
+	{
+		if (options[a] == tosearch)
+			return (a % 3);
+	}
+	return (-1);
+
+}
+
+Form	*makeForm(std::string type, std::string target)
+{
+	Form	*form_types[] = {&ShrubberyCreationForm(), &RobotomyRequestForm(), &PresidentialPardonForm()};
+	int index;
+	
+	index = find_coincidence(str_toLower(type));
+	if (index == -1)
+	{
+		std::cout << "Unknown form \"" << type << "\"" << std::endl;
+		return (NULL);
+	}
+	else
+	{
+		//!		Return pointer to the correct specific Form.
+	}
+}
---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 19:37:18 by jvacaris          #+#    #+#             */
+/*   Updated: 2022/11/29 20:13:44 by jvacaris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+class Intern
+{
+	public:
+		Intern();
+		Intern(Intern &tocopy);
+		~Intern();
+		Intern & operator = (Intern &toequalize);
+		Form *makeForm(std::string type, std::string target);
+};
