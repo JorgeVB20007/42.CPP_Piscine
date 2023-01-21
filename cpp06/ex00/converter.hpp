022b4885da43f3de2d5d1588c9377ab631ff7810
jvacaris <>
2023-01-21 18:13:33 +0100

    cpp06 started

--- cpp06/ex00/converter.hpp
+++ cpp06/ex00/converter.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   changetitle.hpp                                    :+:      :+:    :+:   */
+/*   converter.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 20:53:10 by jvacaris          #+#    #+#             */
-/*   Updated: 2022/11/30 21:54:42 by jvacaris         ###   ########.fr       */
+/*   Updated: 2023/01/21 18:11:18 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define CHANGETITLE_HPP
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+#define MAX_FLOAT 340282346638528859811704183484516925440.0f
+#define MIN_FLOAT -340282346638528859811704183484516925440.0f
 
 enum e_type_of_value
 {
@@ -31,5 +37,11 @@ struct s_str_characteristics
 	bool has_others;
 	bool final_f;
 }	t_str_characteristics;
+
+
+void is_char(std::string str);
+void is_int(std::string str);
+void is_float(std::string str);
+void is_double(std::string str);
 
 #endif
