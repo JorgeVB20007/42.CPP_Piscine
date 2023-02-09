f149b65e1fe385d99c7331963719a2f6dba62c9e
jvacaris <>
2023-02-09 21:27:08 +0100

    .hpp might be done. Missing a good

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -9,12 +9,45 @@ template <class T>
 class Array
 {
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(const Array &tocopy);
-		Array & operator = (const Array &toequalize);
-		~Array();
-		const unsigned int size() const
+		Array(): arr_size(0)
+		{
+			content = new T[0];
+			if (NOTIFS)
+				std::cout << "Array Default constructor called." << std::endl;
+		}
+
+		Array(unsigned int n): arr_size(n)
+		{
+			content = new T[n];
+			if (NOTIFS)
+				std::cout << "Array constructor called." << std::endl;
+		}
+
+		Array(const Array &tocopy): arr_size(tocopy.size())
+		{
+			*this = tocopy;
+			if (NOTIFS)
+				std::cout << "Array constructor called." << std::endl;
+		}
+		Array & operator = (const Array &toequalize)
+		{
+			content = toequalize.content;
+		}
+
+		Array & operator[] (const unsigned int idx)
+		{
+			if (idx >= arr_size)
+				throw Array::ElementOutOfLimits();
+			return (content[idx]);
+		}
+
+//!		~Array();
+
+
+		const unsigned int &size() const
+		{
+			return (arr_size);
+		}
 
 		class ElementOutOfLimits: public std::exception
 		{
@@ -23,41 +56,11 @@ class Array
 				{
 					return("Exception: Attempting to reach out of the Array's limits\n");
 				}
-		}
+		};
 
 	private:
 		const unsigned int arr_size;
 		T	*content;
 };
-
-template <class T>
-Array<T>::Array(): arr_size(0)
-{
-	content = new[0]
-	if (NOTIFS)
-		std::cout << "Array Default constructor called." << std::endl;
-}
-
-template <class T>
-Array<T>::Array(unsigned int n): arr_size(n)
-{
-	content = new[n]
-	if (NOTIFS)
-		std::cout << "Array constructor called." << std::endl;
-}
-
-template <class T>
-Array<T>::Array(const Array &tocopy): arr_size(tocopy.size())
-{
-	*this = tocopy;
-	if (NOTIFS)
-		std::cout << "Array constructor called." << std::endl;
-}
-
-template <class T>
-Array<T> & Array<T>::operator = (const Array &toequalize)
-{
-	content = toequalize.content;
-}
 
 #endif
