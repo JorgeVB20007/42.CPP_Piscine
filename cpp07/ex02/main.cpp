c5fed8927455bd6a8b48ea2769795a1d170eb5b3
jvacaris <>
2023-02-13 19:01:25 +0100

    except this

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:57:51 by jvacaris          #+#    #+#             */
-/*   Updated: 2023/02/13 18:55:28 by jvacaris         ###   ########.fr       */
+/*   Updated: 2023/02/13 19:01:08 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,18 +81,17 @@ int main()
 	megarray[0] = b;
 	megarray[1] = c;
 
-//?		Printing them with the template's print() function
+	//?		Printing them with the template's print() function
 	megarray[0].print();
 	megarray[1].print();
 
-//?		Creating and printing an Array of std::string
+	//?		Creating and printing an Array of std::string
 	Array<std::string> strings(5);
 	strings[0] = "Somebody once told me the world is gonna roll me";
 	strings[1] = "I ain't the sharpest tool in the shed";
 	strings[2] = "She was looking kind of dumb with her finger and her thumb";
 	strings[3] = "In the shape of an \"L\" on her forehead";
 	strings[4] = "Well";
-	
 	strings.print();
 }
 
